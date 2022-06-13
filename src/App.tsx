f8e62bdf8434ad49910fd5cf939765824ac9d7fa
Simon <simon@benezan.de>
2022-06-13 19:52:06 +0200

    default material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,84 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useEffect, useState } from "react";
+import { Routes } from "react-router-dom";
+import LinkBehavior from "./components/LinkBehavior";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import { selectTheme } from "./redux/interfaceSlice";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark"
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: LinkBehavior
+      }
+    }
+  }
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: "light"
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: LinkBehavior
+      }
+    }
+  }
+});
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [muiTheme, setTheme] = useState(darkTheme);
+  const theme = useAppSelector((state) => state.interface.theme);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      dispatch(selectTheme(storedTheme === "light-theme" ? "light-theme" : "dark-theme"));
+    }
+    const userAgent = navigator.userAgent;
+    let operatingSystem: string;
+    if (/android/i.test(userAgent)) {
+      operatingSystem = "android";
+    } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+      operatingSystem = "ios";
+    } else if (/Win/.test(userAgent)) {
+      operatingSystem = "windows";
+    } else if (/Mac/i.test(userAgent)) {
+      operatingSystem = "mac";
+    } else {
+      operatingSystem = "unknown";
+    }
+    const mobile: boolean =
+      (navigator as NewNavigator).userAgentData?.mobile || operatingSystem === "android" || operatingSystem === "ios";
+    console.log(`Mobile: ${mobile}`);
+  }, [dispatch]);
+
+  useEffect(() => {
+    setTheme(theme === "dark-theme" ? darkTheme : lightTheme);
+    if (theme) {
+      localStorage.setItem("theme", theme);
+    }
+  }, [theme]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <Routes></Routes>
+    </ThemeProvider>
   );
+}
+
+interface NewNavigator extends Navigator {
+  userAgentData: {
+    mobile: boolean;
+  };
 }
 
 export default App;
---
+++ src/redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import interfaceReducer from "./interfaceSlice";
+
+export const store = configureStore({
+  reducer: {
+    interface: interfaceReducer
+  }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
